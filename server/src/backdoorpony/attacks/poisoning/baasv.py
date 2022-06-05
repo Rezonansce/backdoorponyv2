f97f32c40a8b54da2cf641e14352d384a4411c35
Kristián Gogora <k.gogora@student.tudelft.nl>
2022-06-05 15:35:28 +0200

    Missing comments

--- server/src/backdoorpony/attacks/poisoning/baasv.py
+++ server/src/backdoorpony/attacks/poisoning/baasv.py
@@ -267,7 +267,23 @@ class BAASV():
         #shutil.rmtree(self.temp_final)
 
     def save_image(self, data, out_shape, save_path):
+        """
+        Converts audio data to spectrogrammer image data
 
+        Parameters
+        ----------
+        data : array (n,)
+            audio data.
+        out_shape : touple (n, n)
+            image resolution.
+        save_path : string
+            path to the save location.
+
+        Returns
+        -------
+        None.
+
+        """
 
 
         noverlap=16
@@ -285,9 +301,37 @@ class BAASV():
         plt.close(fig)
 
     def load_image(self, dummy_file_path):
+        """
+        Loads the previosuly saved image
+
+        Parameters
+        ----------
+        dummy_file_path : string
+            path to the image location.
+
+        Returns
+        -------
+        TYPE
+            image data with shape 'self.output_shape'.
+
+        """
         return matplotlib.pyplot.imread(dummy_file_path)[:,:,0]
 
 def path_leaf(path):
+    """
+    Method used for retrieving filenames.
+
+    Parameters
+    ----------
+    path : string
+        path to the file.
+
+    Returns
+    -------
+    string
+        filename.
+
+    """
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
