bc771eb7d1d36b7e9427374697cefc50f8c3729a
Oliver Sihlovec <o.sihlovec@student.tudelft.nl>
2022-06-11 18:34:26 +0200

    Add gpu support for GraphClassifierNew tests

--- server/src/tests/classifiers/test_GraphClassifierNew.py
+++ server/src/tests/classifiers/test_GraphClassifierNew.py
@@ -22,6 +22,7 @@ class TestDataLoader(TestCase):
                     scheduler.return_value = "scheduler"
                     model = MagicMock(name='model')
                     model.parameters.return_value = "params"
+                    model.to.return_value=model
 
                     # Act
                     classifier = GraphClassifier(model=model, criterion = CrossEntropyLoss)
@@ -50,6 +51,8 @@ class TestDataLoader(TestCase):
                 model.train.return_value = "train"
                 model.forward.return_value = tensor([1.0, 2.0, 3.0, 4.0])
                 model.return_value = tensor([1.0, 2.0, 3.0, 4.0])
+                model.to.return_value=model
+
                 
                 loss_fn = Mock()
                 loss = Mock(name = "loss")
@@ -81,6 +84,7 @@ class TestDataLoader(TestCase):
                         model.eval.return_value = "eval"
                         model.forward.return_value = tensor([1.0, 2.0, 3.0, 4.0])
                         model.return_value = tensor([1.0, 2.0, 3.0, 4.0])
+                        model.to.return_value=model
                         
                         loss_fn = Mock()
                         loss = Mock(name = "loss")
