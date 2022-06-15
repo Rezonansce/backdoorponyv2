cd888329386bafb5d775bf05aab5c2e0ae9e2b63
Nikita Smolin <N.Smolin@student.tudelft.nl>
2022-06-15 11:41:54 +0200

    chdir moved to actual methods

--- server/src/tests/datasets/test_IMDB.py
+++ server/src/tests/datasets/test_IMDB.py
@@ -14,8 +14,7 @@ class TestIMDB(TestCase):
     def __init__(self, *args, **kwargs):
         super(TestIMDB, self).__init__(*args, **kwargs)
         self.imdb = IMDB()
-        # change to directory used when running for proper imports
-        os.chdir(os.path.expanduser(os.path.dirname(datasets.__file__)))
+        print("current dir ", os.getcwd())
 
     def test_clean(self):
         # test data cleaning
@@ -63,13 +62,16 @@ class TestIMDB(TestCase):
 
     def test_get_data(self):
         # test that only a fraction of data is loaded with correct length
-        print("current dir ", os.getcwd())
+        # change to directory used when running for proper imports
+        os.chdir(os.path.expanduser(os.path.dirname(datasets.__file__)))
         train_data, test_data = self.imdb.get_data(0.01, 0.001)
         self.assertTrue(len(train_data) == 250)
         self.assertTrue(len(test_data) == 25)
 
     def test_get_datasets(self):
         # test that only a fraction of data is loaded with correct padded sequence length
+        # change to directory used when running for proper imports
+        os.chdir(os.path.expanduser(os.path.dirname(datasets.__file__)))
         data_train, data_test, _ = self.imdb.get_datasets(0.1, 0.01)
         self.assertTrue(np.shape(data_train[0]) == (2500, 700))
         self.assertTrue(np.shape(data_test[0]) == (250, 700))
